46541477d7b04522a96b9365b73e66bcbe24d521
Matthew Walsh <walsh9@gmail.com>
2014-10-17 00:00:37 -0400

    prototype 2

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 angular.module('tamaedit', [])
-  .controller('MainCtrl', ['$scope', function($scope){
-    $scope.pixels = [
-      [ true, false, false, false, false],
-      [false,  true, false, false, false],
-      [false, false,  true, false, false],
-      [false, false, false,  true, false],
-      [false, false, false, false,  true]
-    ];
+  .controller('MainCtrl', ['$scope', function($scope) {
+    var numFrames = 4;
+    var numRows = 16;
+    var numColumns = 32;
+    $scope._ = _;
+    $scope.frames = _.range(0, numFrames).map(function () {
+      var rows = _.range(0, numRows).map(function () {
+        var pixels = _.range(0, numColumns).map(function () {
+          return false;
+        });
+        return pixels;
+      });
+      return rows;
+    });
+    console.log($scope.frames);
     togglePixel = function (pixel, scope) {
       var x = pixel.getAttribute('data-index').split(',')[0];
       var y = pixel.getAttribute('data-index').split(',')[1];
-      scope.pixels[y][x] = !scope.pixels[y][x];
+      var f = pixel.getAttribute('data-index').split(',')[2];
+      scope.frames[f][y][x] = !scope.frames[f][y][x];
     };
     $scope.clickHandler = function (event) {
       togglePixel(event.target, $scope);
