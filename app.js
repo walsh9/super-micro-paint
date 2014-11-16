8b26995c3e63cec7f672fe53aa43e25472a560f4
walsh9 <walsh9@gmail.com>
2014-11-16 18:28:46 -0500

    Fast mouse movement filled in with connected lines using Bresenham algorithm

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ angular.module('super-micro-paint', [])
     // }
     $scope.pen = false;
     $scope.penmode = false;
+    $scope.lastPixel = {};
     $scope.range = function(n) {
         return new Array(n);
     };
@@ -36,14 +37,27 @@ angular.module('super-micro-paint', [])
       var x = Math.floor(pixel.getAttribute('data-index').split(',')[0]);
       var y = Math.floor(pixel.getAttribute('data-index').split(',')[1]);
       var f = Math.floor(pixel.getAttribute('data-index').split(',')[2]);
-      console.log(x,y,mode);
-      console.log(scope.frames[f].set(x, y, mode));
+      var currentFrame = scope.frames[f];
+      scope.frames[f].set(x, y, mode);
+      //currentFrame.forLine(x, y, x+5, y+5, function(i,x,y) {currentFrame.set(x, y, true);});
     };
+    var setLine = function(pixel0, pixel1, mode, scope) {
+      var x0 = Math.floor(pixel0.getAttribute('data-index').split(',')[0]);
+      var y0 = Math.floor(pixel0.getAttribute('data-index').split(',')[1]);
+      var x1 = Math.floor(pixel1.getAttribute('data-index').split(',')[0]);
+      var y1 = Math.floor(pixel1.getAttribute('data-index').split(',')[1]);
+      var f = Math.floor(pixel0.getAttribute('data-index').split(',')[2]);
+      var currentFrame = scope.frames[f];
+      currentFrame.forLine(x0, y0, x1, y1, function(val, x, y) {currentFrame.set(x, y, mode);});
+    };     
     var getPixel = function(pixel, scope) {
       var x = Math.floor(pixel.getAttribute('data-index').split(',')[0]);
       var y = Math.floor(pixel.getAttribute('data-index').split(',')[1]);
       var f = Math.floor(pixel.getAttribute('data-index').split(',')[2]);
       return scope.frames[f].get(x, y);
+    };
+    var drawLine = function(pixel, pixel2, scope) {
+
     };
     var togglePixel = function (pixel, scope) {
       setPixel(pixel, !getPixel(pixel, scope), scope);
@@ -52,15 +66,21 @@ angular.module('super-micro-paint', [])
       $scope.pen = true;
       $scope.penmode = !getPixel(event.target, $scope);
       togglePixel(event.target, $scope);
+      $scope.lastPixel = event.target;
       return false;
     };
     $scope.penOver = function (event) {
       if ($scope.pen) {
-        setPixel(event.target, $scope.penmode, $scope);
+        if ($scope.lastPixel) {
+          setLine($scope.lastPixel, event.target, $scope.penmode, $scope);          
+        }
+        $scope.lastPixel = event.target;
+        //setPixel(event.target, $scope.penmode, $scope);
       }
     };
     $scope.penUp = function (event) {
       $scope.pen = false;
+      $scope.lastPixel = {};
       clearSelection();
     };
 }]);
--- array2d.js
+++ array2d.js
@@ -19,7 +19,6 @@ Array2d.prototype.get = function (x, y) {
     return this.rawArray[x + y * this.width];
 };
 Array2d.prototype.set = function (x, y, val) {
-    console.log(x, y, val, x + y * this.width);
     this.rawArray[x + y * this.width] = val;
     return this;
 };
@@ -42,20 +41,46 @@ Array2d.prototype.map = function (callback, thisArg) {
     });
     return mappedArray;
 };
-Array2d.prototype.slice2d = function(x1, y1, x2, y2) {
+Array2d.prototype.slice2d = function(x0, y0, x1, y1) {
     var arraySlice = [];
     var x, y;
-    x2 = x2 || this.width;
-    y2 = y2 || this.height;
-    for (y = y1; y < y2; y++) {
-        for (x = x1; x < x2; x++) {
+    x1 = x1 || this.width;
+    y1 = y1 || this.height;
+    for (y = y0; y < y1; y++) {
+        for (x = x0; x < x1; x++) {
             arraySlice.push(this.get(x, y));
         }
     }
-    return new Array2d(x2 - x1, y2 - y1, arraySlice);
+    return new Array2d(x1 - x0, y1 - y0, arraySlice);
 };
 Array2d.prototype.clone = function() {
     return this.slice2d(0,0);
+};
+Array2d.prototype.do = function (x, y, callback, thisArg) {
+    callback.call(thisArg, this.rawArray[x + y * this.width], x, y, this.width, this.height);
+    return this;
+};
+Array2d.prototype.forLine = function(x0, y0, x1, y1, callback, thisArg) {
+    var deltaX = Math.abs(x1 - x0);
+    var deltaY = -Math.abs(y1 - y0);
+    var signX = x0 < x1 ? 1 : -1;
+    var signY = y0 < y1 ? 1 : -1;
+    var err = deltaX + deltaY;
+    var e2;
+    this.do(x0, y0, callback, thisArg);
+    while (!(x0 == x1 && y0 == y1)) {
+        e2 = Math.floor(err * 2);
+        if (e2 >= deltaY) { 
+            err += deltaY; 
+            x0 += signX; 
+        }
+        if (e2 <= deltaX) { 
+            err += deltaX; 
+            y0 += signY; 
+        }
+        console.log(x0, y0, e2);
+        this.do(x0, y0, callback, thisArg);
+    }
 };
 Array2d.prototype.fill = function(value) {
     var w = this.width;
